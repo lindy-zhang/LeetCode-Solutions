df7ec76a8595a2ede2f21d245c44ecab4ff67b01
Lindy Zhang <144083073+lindy-zhang@users.noreply.github.com>
2026-01-19 17:21:41 -0500

    Create rotate-image.py

---
+++ Medium/rotate-image.py
@@ -0,0 +1,21 @@
+Medium/rotate-image.py
+
+# Notes:
+# - Matrix manipulation
+# - 2 steps: transpose, then horizontal reflection
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+
+        # Transpose: swap i, js
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        # Horizontal reflection
+        for i in range(n):
+            for j in range(n//2):
+                matrix[i][j], matrix[i][n-j-1] = matrix[i][n-j-1], matrix[i][j]
