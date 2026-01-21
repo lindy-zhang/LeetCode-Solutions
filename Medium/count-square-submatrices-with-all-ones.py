7f60963b318d4bde0db953db124e7b2ad5488b7d
Lindy Zhang <144083073+lindy-zhang@users.noreply.github.com>
2026-01-21 16:15:18 -0500

    Update count-square-submatrices-with-all-ones.py

--- Medium/count-square-submatrices-with-all-ones.py
+++ Medium/count-square-submatrices-with-all-ones.py
@@ -25,3 +25,33 @@ class Solution:
             for j in range(n):
                 count += memo[(i, j)]
         return count
+
+
+# Optimal solution: O(n)
+class Solution:
+    def countSquares(self, matrix: List[List[int]]) -> int:
+        m, n = len(matrix), len(matrix[0])
+        # dp[j] stores the largest square side length ending at the current row, column j
+        dp = [0] * n
+        total_squares = 0
+        prev_diag = 0 # This stores the (i-1, j-1) value
+        
+        for i in range(m):
+            for j in range(n):
+                temp = dp[j] # Save the 'above' value before it gets overwritten
+                
+                if matrix[i][j] == 1:
+                    if i == 0 or j == 0:
+                        dp[j] = 1
+                    else:
+                        # dp[j] is 'above', dp[j-1] is 'left', prev_diag is 'diagonal'
+                        dp[j] = min(dp[j], dp[j-1], prev_diag) + 1
+                    
+                    total_squares += dp[j]
+                else:
+                    dp[j] = 0
+                
+                # The 'above' value we saved becomes the 'diagonal' for the next column
+                prev_diag = temp
+                
+        return total_squares
